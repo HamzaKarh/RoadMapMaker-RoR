f557a4938e5ae63ed8ba5abde3c13dab8e2ec7da
Hamza Karhat <karhat.hamza1@gmail.com>
2020-04-19 18:22:19 +0200

    Project column addition

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -73,6 +73,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :client_company_name, :client_name, :description, :user)
+      params.require(:project).permit(:name, :client_company_name, :client_name, :description, :team, :deliverable, :budget, :extworkforce)
     end
 end
